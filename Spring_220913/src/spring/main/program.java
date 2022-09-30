3c009a861d9d469cae43ddcb8f4afc5f50e6a385
juyangjin <wndid2008@gmail.com>
2022-09-30 19:52:01 +0900

    2022/09/30

---
+++ Spring_220913/src/spring/main/program.java
@@ -0,0 +1,12 @@
+package spring.main;
+
+public class program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Exam _mid = new MidExam();  //부모에서부터 형변환이 되어 상속을 받음
+		Exam _final = new FinalExam();
+
+	}
+
+}
---
+++ Spring_220927/src/spring/main/JavaConfig.java
@@ -0,0 +1,7 @@
+package spring.main;
+
+public class JavaConfig {
+	
+	
+
+}
