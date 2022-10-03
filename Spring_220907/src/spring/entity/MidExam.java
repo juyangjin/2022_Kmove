96eb9db92a13066e97b7d95270c5897c02614676
juyangjin <wndid2008@gmail.com>
2022-10-03 20:40:43 +0900

    Update MidExam.java

--- Spring_220907/src/spring/entity/MidExam.java
+++ Spring_220907/src/spring/entity/MidExam.java
@@ -1,5 +1,17 @@
 package spring.entity;
 
-public class MidExam {
+public class MidExam implements Exam {
+
+	@Override
+	public int Total() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public float Average() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
